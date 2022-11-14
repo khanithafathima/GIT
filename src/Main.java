59ef296f1540a937781d99937ff3f29dbde3e3b0
zakirasim <118193382+zakirasim@users.noreply.github.com>
2022-11-14 11:25:44 -0500

    here assigned x value

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
+        int x = 20;
         System.out.println("Hello world!");
 
         System.out.println("this is me");
+        System.out.println( x);
+
     }
 }
